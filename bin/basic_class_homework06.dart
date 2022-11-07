47851234c91d3b999feae6724681a7af53f31f6e
mehroj <mehrojgaybullayev49@gmail.com>
2022-11-07 14:53:20 +0500

    class

--- bin/basic_class_homework06.dart
+++ bin/basic_class_homework06.dart
@@ -3,5 +3,17 @@
 /*
 Create a "Person" class that has a name("name") and a age("age")
 */
+class Person {
+  String name = 'Mehroj';
+  int age = 22;
+  Person({required String name, required int age}) {
+    this.name = name;
+    this.age = age;
+  }
+}
 
-void main() {}
+void main() {
+  Person x = Person(age: 25, name: 'Ali');
+  print(x.age);
+  print(x.name);
+}
--- bin/basic_class_homework07.dart
+++ bin/basic_class_homework07.dart
@@ -4,5 +4,17 @@
 Create a "Person" class that has a name("name") and a age("age")
 create an object named "person" whose name is "Ali", age is "25"
 */
+class Person {
+  String name = 'Mehroj';
+  int age = 22;
+  Person({required String name, required int age}) {
+    this.name = name;
+    this.age = age;
+  }
+}
 
-void main() {}
+void main() {
+  Person x = Person(age: 25, name: 'Ali');
+  print(x.age);
+  print(x.name);
+}
